fa85817840bf0b3741320ae0c5c1f8ebf2efe048
iquesadmor <iquesadmor@educacion.navarra.es>
2025-12-16 09:40:50 +0100

    Trabajador main ivan

--- src/main/java/Trabajador.java
+++ src/main/java/Trabajador.java
@@ -46,6 +46,9 @@ public class Trabajador {
     public static void main(String[] args) {
         Trabajador t1 = new Trabajador("Manolo");
         System.out.println(t1);
+        Trabajador t2 = new Trabajador("EloyCalvo");
+        t2.setCategoria(Ctes.CAT_JUNIOR);
+        System.out.println(t2);
     }
 
 }
